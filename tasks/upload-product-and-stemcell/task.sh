4078c35aee7cbbc0f894da76707de15ab8caeb4f
Stuart Charlton <scharlton@pivotal.io>
2017-06-15 15:05:01 -0400

    Adding echos for logging task

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -4,9 +4,11 @@ if [[ ! -z "$NO_PROXY" ]]; then
   echo "$OM_IP $OPS_MGR_HOST" >> /etc/hosts
 fi
 
+echo "Checking for needed stemcell in metadata"
 STEMCELL_VERSION=`cat ./pivnet-product/metadata.json | jq --raw-output '.Dependencies[] | select(.Release.Product.Name | contains("Stemcells")) | .Release.Version'`
 
 if [ -n "$STEMCELL_VERSION" ]; then
+  echo "Stemcell not found in metadata; checking Ops Manager diagnostic report"
   diagnostic_report=$(
     om-linux \
       --target https://$OPS_MGR_HOST \
@@ -41,7 +43,10 @@ if [ -n "$STEMCELL_VERSION" ]; then
     echo "Removing downloaded stemcell $STEMCELL_VERSION"
     rm $SC_FILE_PATH
   fi
+  
+  echo "Finished stemcells"
 fi
 
+echo "Uploading product"
 FILE_PATH=`find ./pivnet-product -name *.pivotal`
 om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k upload-product -p $FILE_PATH

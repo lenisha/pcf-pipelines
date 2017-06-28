aae646ecc618c681db133637adf382018a3e096d
Elena Neros <lenisha@gmail.com>
2017-06-28 10:12:56 -1200

    add resources sso

--- tasks/config-sso/task.sh
+++ tasks/config-sso/task.sh
@@ -13,21 +13,16 @@ PRODUCT_VERSION=`echo $TILE_RELEASE | cut -d"|" -f3 | tr -d " "`
 
 om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k stage-product -p $PRODUCT_NAME -v $PRODUCT_VERSION
 
-function fn_get_azs {
-     local azs_csv=$1
-     echo $azs_csv | awk -F "," -v braceopen='{' -v braceclose='}' -v name='"name":' -v quote='"' -v OFS='"},{"name":"' '$1=$1 {print braceopen name quote $0 quote braceclose}'
-}
-
 TILE_AVAILABILITY_ZONES=$(fn_get_azs $TILE_AZS_SSO)
 
 
 NETWORK=$(cat <<-EOF
 {
   "singleton_availability_zone": {
-    "name": "$TILE_AZS_SSO"
+    "name": "$TILE_AZ_SSO_SINGLETON"
   },
   "other_availability_zones": [
-    $TILE_AZS_SSO
+    $TILE_AVAILABILITY_ZONES
   ],
   "network": {
     "name": "$NETWORK_NAME"
@@ -36,7 +31,18 @@ NETWORK=$(cat <<-EOF
 EOF
 )
 
+RESOURCES=$(cat <<-EOF
+{
+  "deploy-service-broker": {
+    "instance_type": {"id": "automatic"},
+    "instances" : 1
+  },
+  "destroy-broker": {
+    "instance_type": {"id": "automatic"},
+    "instances" : 1
+  }
+}
+EOF
+)
 
-
-
-om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_NAME  -pn "$NETWORK"
+om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_NAME  -pn "$NETWORK" -pr "$RESOURCES"

68d34a6f0b780a3611aa10126b1ce520bdb4c99a
Elena Neros <lenisha@gmail.com>
2017-06-28 10:15:01 -1200

    add resources sso

--- tasks/config-sso/task.sh
+++ tasks/config-sso/task.sh
@@ -13,6 +13,11 @@ PRODUCT_VERSION=`echo $TILE_RELEASE | cut -d"|" -f3 | tr -d " "`
 
 om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k stage-product -p $PRODUCT_NAME -v $PRODUCT_VERSION
 
+function fn_get_azs {
+     local azs_csv=$1
+     echo $azs_csv | awk -F "," -v braceopen='{' -v braceclose='}' -v name='"name":' -v quote='"' -v OFS='"},{"name":"' '$1=$1 {print braceopen name quote $0 quote braceclose}'
+}
+
 TILE_AVAILABILITY_ZONES=$(fn_get_azs $TILE_AZS_SSO)
 
 
